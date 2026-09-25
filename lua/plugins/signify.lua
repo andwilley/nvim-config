2798735c2d1b69847d270f66843759fb0c8878b0
Drew Willey <andwilley@gmail.com>
2026-09-25 16:09:23 +0000

    shortcut for signify diff

--- lua/plugins/signify.lua
+++ lua/plugins/signify.lua
@@ -1,5 +1,8 @@
 return {
   "msuozzo/vim-signify",
+  keys = {
+    { "<leader>dd", "<cmd>SignifyDiff<cr>", desc = "Signify: Toggle Diff Mode" },
+  },
   init = function ()
     vim.opt.updatetime = 100
   end,
